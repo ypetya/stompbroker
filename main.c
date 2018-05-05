1d59059ab138a8c8f9bc75f4103bbeaef90bd319
Mihaly K <mic00004@gmail.com>
2018-05-05 23:03:19 +0200

    working, plus usleep

--- main.c
+++ main.c
@@ -18,24 +18,30 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <arpa/inet.h> // warning inet_ntoa declared here
 #include <sys/wait.h>
 #include <fcntl.h> /* Added for the nonblocking socket */
+#include <unistd.h> // for usleep
+#include <errno.h>
 
 #define MYPORT 3456    /* the port users will be connecting to */
 #define BACKLOG 10     /* how many pending connections queue will hold */
 
-main()
+int main()
 {
     int 			sockfd, new_fd;  /* listen on sock_fd, new connection on new_fd */
     struct 	sockaddr_in 	my_addr;    /* my address information */
     struct 	sockaddr_in 	their_addr; /* connector's address information */
-    int 			sin_size;
+    //int 			sin_size;
+    socklen_t		sin_size;
     char			string_read[255];
+    char			string_write[255];
     int 			n,i;
     int			last_fd;	/* Thelast sockfd that is connected	*/
 
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         perror("socket");
+        printf("error: %s",strerror(errno));
         exit(1);
     }
 
@@ -46,63 +52,86 @@ main()
     my_addr.sin_addr.s_addr = INADDR_ANY; /* auto-fill with my IP */
     bzero(&(my_addr.sin_zero), 8);        /* zero the rest of the struct */
 
-    if (bind(sockfd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr)) == -1) {
-        perror("bind");
-        exit(1);
-    }
-
 //?? place sure
     int enable = 1;
     if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
-        error("setsockopt(SO_REUSEADDR) failed");
+        printf("setsockopt(SO_REUSEADDR) failed");
+
+    if (bind(sockfd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr)) == -1) {
+        perror("bind");
+        printf("error: %s",strerror(errno));
+        exit(1);
+    }
+
 
 
 
     if (listen(sockfd, BACKLOG) == -1) {
         perror("listen");
+        printf("error: %s",strerror(errno));
+
         exit(1);
     }
-
+/*
+    sin_size = sizeof(struct sockaddr);
     if ((new_fd = accept(sockfd, (struct sockaddr *)&their_addr,&sin_size)) == -1) {
-        perror("accept");
-    }
+        perror("accept1");
+        printf("error: %s",strerror(errno));
 
-printf("last_fd= %d\n",last_fd);
-printf("sockfd= %d\n",sockfd);
-printf("new_fd= %d\n",new_fd);
+    }
+*/
+//printf("last_fd= %d\n",last_fd);
+//printf("sockfd= %d\n",sockfd);
+//printf("new_fd= %d\n",new_fd);
 
 
 
     fcntl(last_fd, F_SETFL, O_NONBLOCK); /* Change the socket into non-blocking state	*/
-    fcntl(new_fd, F_SETFL, O_NONBLOCK); /* Change the socket into non-blocking state	*/
+//    fcntl(new_fd, F_SETFL, O_NONBLOCK); /* Change the socket into non-blocking state	*/
 
     while(1){
         for (i=sockfd;i<=last_fd;i++){
-            printf("Round number %d\n",i);
-            if (i = sockfd){
+            //printf("Round number %d\n",i);
+            if (i == sockfd){
                 sin_size = sizeof(struct sockaddr_in);
                 if ((new_fd = accept(sockfd, (struct sockaddr *)&their_addr,&sin_size)) == -1) {
-                    perror("accept");
+                    //perror("accept2");
+                    //printf("error: %s",strerror(errno));
+                }else{
+                    printf("server: got connection from %s\n", (char *)inet_ntoa(their_addr.sin_addr));
+                    fcntl(new_fd, F_SETFL, O_NONBLOCK);
+                    last_fd = new_fd;
+                    //printf("new_fd= %d\n",new_fd);
+
                 }
-                printf("server: got connection from %s\n", inet_ntoa(their_addr.sin_addr));
-                fcntl(new_fd, F_SETFL, O_NONBLOCK);
-                last_fd = new_fd;
             }
             else{
-                n=recv(new_fd,string_read,sizeof(string_read),0);
+                n=recv(i,string_read,sizeof(string_read),0);
                 if (n < 1){
-                    perror("recv - non blocking \n");
-                    printf("Round %d, and the data read size is: n=%d \n",i,n);
+                    //if(errno == EWOULDBLOCK){printf("EWOULDBLOCK");}
+                    //if(errno == EAGAIN){printf("EAGAIN   ");}
+                    if(errno != EWOULDBLOCK){
+                        perror("recv - non blocking \n");
+                        printf("Round %d, and the data read size is: n=%d \n",i,n);
+                    }
                 }
                 else{
                     string_read[n] = '\0';
                     printf("The string is: %s \n",string_read);
-                    if (send(new_fd, "Hello, world!\n", 14, 0) == -1)
+                    if(strlen(string_read) < (sizeof(string_write) - 10) ){
+                        sprintf(string_write,"You said:%s\n",string_read);
+                    }else{
+                        strcpy(string_write,"Jabber...\n");
+
+                        }
+                    if (send(i, string_write, strlen(string_write), 0) == -1)
                         perror("send");
                 }
             }
         }
+        usleep(1000000);
     }
 
+    return 0;
 }
 
