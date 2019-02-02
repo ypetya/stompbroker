16d74b2bf32fa00913814056161028e91b068cfe
Peter Kiss <ypetya@gmail.com>
2019-02-02 11:55:45 +0100

    Reconnect nodejs

--- examples/js/websocket/stomp.diag.stale.js
+++ examples/js/websocket/stomp.diag.stale.js
@@ -2,19 +2,41 @@
 
 const Stomp = require('stompjs');
 
-const sock = Stomp.overWS('ws://localhost:3490');
-
-//sock.debug = console.log;
+const wsUrl = 'ws://localhost:3490';
 const topic = `DIAG/${Math.random()}`;
 console.log(Date.now(), topic);
-sock.connect([], () => {
-    sock.subscribe(topic, msg => {
-        const input=msg.headers["input-queue-size"];
-        const output=msg.headers["output-queue-size"];
-        const stale=msg.headers["stale-queue-size"];
-        const latency = Date.now() - Number(msg.headers.ts);
-        console.log(`latency: ${latency}, input-queue-size: ${input}, output-queue-size:${output}, stale-queue-size:${stale}`);
+
+let sock = null;
+let connected = false;
+
+setInterval(() => {
+        if(connected) 
+            sock.send(topic, { ts: Date.now() }, 'stale')
+}, 500);
+
+const connect = () => {
+    sock = Stomp.overWS(wsUrl);
+    //sock.debug = console.log;
+        
+    sock.connect([], () => {
+        sock.subscribe(topic, msg => {
+            const input=msg.headers["input-queue-size"];
+            const output=msg.headers["output-queue-size"];
+            const stale=msg.headers["stale-queue-size"];
+            const latency = Date.now() - Number(msg.headers.ts);
+            console.log(`latency: ${latency}, input-queue-size: ${input}, output-queue-size:${output}, stale-queue-size:${stale}`);
+        });
+        connected=true;
+        console.log(`Connected to ${wsUrl}`);
+    }, error => {
+        connected=false;
+        console.error(error);
     });
-    setInterval(() =>
-        sock.send(topic, { ts: Date.now() }, 'stale'), 500);
-}, console.error);
+}
+
+setInterval(()=>{
+    if(!connected){
+        console.log('Connecting ...');
+        connect();
+    }
+}, 1000);
